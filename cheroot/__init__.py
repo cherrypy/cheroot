c371e2b7264ee3461f64e15bc5aafa11e4108302
Robert Brewer <fumanchu@aminus.org>
2012-03-12 16:50:59 -0700

    Moved a docstring.

--- cheroot/__init__.py
+++ cheroot/__init__.py
@@ -1,4 +1,29 @@
-"""Cheroot is a highly-optimized, pure-python HTTP server."""
+"""A high-speed, production ready, thread pooled, generic HTTP server.
+
+Simplest example on how to use this module directly::
+
+    import cheroot
+    
+    def my_crazy_app(environ, start_response):
+        status = '200 OK'
+        response_headers = [('Content-type','text/plain')]
+        start_response(status, response_headers)
+        return ['Hello world!']
+    
+    server = wsgi.WSGIServer(
+                ('0.0.0.0', 8070),
+                server_name='www.cheroot.example',
+                wsgi_app=my_crazy_app)
+    server.start()
+
+The Cheroot HTTP server can serve as many WSGI applications 
+as you want in one instance by using a WSGIPathInfoDispatcher::
+    
+    d = wsgi.WSGIPathInfoDispatcher({'/': my_crazy_app, '/blog': my_blog_app})
+    server = wsgi.WSGIServer(('0.0.0.0', 80), wsgi_app=d)
+
+Want SSL support? Just set server.ssl_adapter to an SSLAdapter instance.
+"""
 
 __version__ = "3.3.0alpha"
 
--- cheroot/server.py
+++ cheroot/server.py
@@ -1,28 +1,4 @@
-"""A high-speed, production ready, thread pooled, generic HTTP server.
-
-Simplest example on how to use this module directly::
-
-    import cheroot
-    
-    def my_crazy_app(environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-type','text/plain')]
-        start_response(status, response_headers)
-        return ['Hello world!']
-    
-    server = wsgi.WSGIServer(
-                ('0.0.0.0', 8070),
-                server_name='www.cheroot.example',
-                wsgi_app=my_crazy_app)
-    server.start()
-
-The Cheroot HTTP server can serve as many WSGI applications 
-as you want in one instance by using a WSGIPathInfoDispatcher::
-    
-    d = WSGIPathInfoDispatcher({'/': my_crazy_app, '/blog': my_blog_app})
-    server = WSGIServer(('0.0.0.0', 80), wsgi_app=d)
-
-Want SSL support? Just set server.ssl_adapter to an SSLAdapter instance.
+"""The HTTP server.
 
 For those of you wanting to understand internals of this module, here's the
 basic call flow. The server's listening thread runs a very tight loop,
