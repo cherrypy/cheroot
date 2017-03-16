2eecc67402bfcd30474223c6d092f8bb475e0680
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-03-17 00:52:57 +0200

    PEP257fy codebase: eliminated D204 pep257 linter error

--- cheroot/errors.py
+++ cheroot/errors.py
@@ -7,14 +7,14 @@ class MaxSizeExceeded(Exception):
 
 
 class NoSSLError(Exception):
-
     """Exception raised when a client speaks HTTP to an HTTPS socket."""
+
     pass
 
 
 class FatalSSLAlert(Exception):
-
     """Exception raised when the SSL implementation signals a fatal alert."""
+
     pass
 
 
