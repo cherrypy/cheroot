cherrypy/cheroot

commit 51c004ac657878de7712accefd985dbbf927fe1e

Improve unicode in URI check under Python 2

cheroot/server.py
@@ -61,7 +61,7 @@ from six.moves import queue
 from six.moves import urllib
 
 from . import errors, __version__
-from ._compat import ntob, unquote_to_bytes
+from ._compat import ntob, bton, ntou, unquote_to_bytes
 from .workers import threadpool
 from .makefile import MakeFile
 
@@ -734,6 +734,10 @@ class HTTPRequest(object):
             try:
                 # https://tools.ietf.org/html/rfc7230#section-5.3.1 (origin_form) and
                 # https://tools.ietf.org/html/rfc7230#section-5.3.2 (absolute form)
+                if six.PY2:  # FIXME: Figure out better way to do this
+                    # Ref: https://stackoverflow.com/a/196392/595220 (like this?)
+                    """This is a dummy check for unicode in URI."""
+                    ntou(bton(uri, 'ascii'), 'ascii')
                 scheme, authority, path, qs, fragment = urllib.parse.urlsplit(uri)
             except UnicodeError:
                 self.simple_response('400 Bad Request', 'Malformed Request-URI')

commit 8dd412a0f0317efbe1c62b26f607cae8ab7f929e

Recode URI using UTF-8 in test.helper
Fix URI encoding issue introduced in #39

cheroot/test/helper.py
@@ -17,7 +17,7 @@ import six
 import cheroot.server
 import cheroot.wsgi
 
-from cheroot._compat import HTTPSConnection, ntob
+from cheroot._compat import HTTPSConnection, ntob, bton
 from cheroot.test import webtest
 
 log = logging.getLogger(__name__)
@@ -156,7 +156,15 @@ class Controller(object):
     def __call__(self, environ, start_response):
         req, resp = Request(environ), Response()
         try:
-            handler = getattr(self, environ['PATH_INFO'].lstrip('/').replace('/', '_'))
+            # Recode handler name as Python supports unicode method names
+            # and we should try match those first.
+            # Source of the idea: cherrypy._cpwsgi.AppResponse.recode_path_qs
+            latin1_handler_name = environ['PATH_INFO'].lstrip('/').replace('/', '_')
+            try:
+                unicode_handler_name = bton(ntob(latin1_handler_name), 'utf-8')
+                handler = getattr(self, unicode_handler_name)
+            except (UnicodeError, AttributeError):
+                handler = getattr(self, latin1_handler_name)
         except AttributeError:
             resp.status = '404 Not Found'
         else: