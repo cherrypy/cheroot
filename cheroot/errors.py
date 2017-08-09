a01c67ba6dba7ed841a87391959429a58dbadc33
Safihre <safihre@sabnzbd.org>
2017-08-09 09:49:35 +0300

    Correctly catch EPROTOTYPE on macOS

--- cheroot/errors.py
+++ cheroot/errors.py
@@ -51,5 +51,5 @@ socket_errors_nonblocking = plat_specific_errors(
     'EAGAIN', 'EWOULDBLOCK', 'WSAEWOULDBLOCK')
 
 if sys.platform == 'darwin':
-    socket_errors_to_ignore.append(plat_specific_errors('EPROTOTYPE'))
-    socket_errors_nonblocking.append(plat_specific_errors('EPROTOTYPE'))
+    socket_errors_to_ignore.extend(plat_specific_errors('EPROTOTYPE'))
+    socket_errors_nonblocking.extend(plat_specific_errors('EPROTOTYPE'))
