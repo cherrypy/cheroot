c8cf6704253d88e3d1a5e479b37b28902adac8ac
Jason R. Coombs <jaraco@jaraco.com>
2018-05-17 10:33:48 -0400

    Extract arg spec as a data structure

--- cheroot/cli.py
+++ cheroot/cli.py
@@ -127,56 +127,83 @@ def parse_wsgi_bind_addr(bind_addr_string):
     return BindLocation(file_socket=bind_addr_string)
 
 
+_arg_spec = {
+    '_wsgi_app': dict(
+        metavar='APP_MODULE',
+        type=str,
+        help=(
+            'WSGI application callable or cheroot.server.Gateway subclass to '
+            'use'
+        ),
+    ),
+    '--bind': dict(
+        metavar='ADDRESS',
+        dest='_bind_addr',
+        type=str,
+        default='127.0.0.1:8000',
+        help='Network interface to listen on (default: 127.0.0.1:8000)',
+    ),
+    '--chdir': dict(
+        metavar='PATH',
+        type=os.chdir,
+        help='Set the working directory',
+    ),
+    '--server-name': dict(
+        dest='server_name',
+        type=str,
+        help='Web server name to be advertised via Server HTTP header',
+    ),
+    '--threads': dict(
+        metavar='INT',
+        dest='numthreads',
+        type=int,
+        help='Minimum number of worker threads',
+    ),
+    '--max-threads': dict(
+        metavar='INT',
+        dest='max',
+        type=int,
+        help='Maximum number of worker threads',
+    ),
+    '--timeout': dict(
+        metavar='INT',
+        dest='timeout',
+        type=int,
+        help='Timeout in seconds for accepted connections',
+    ),
+    '--shutdown-timeout': dict(
+        metavar='INT',
+        dest='shutdown_timeout',
+        type=int,
+        help='Time in seconds to wait for worker threads to cleanly exit',
+    ),
+    '--request-queue-size': dict(
+        metavar='INT',
+        dest='request_queue_size',
+        type=int,
+        help='Maximum number of queued connections',
+    ),
+    '--accepted-queue-size': dict(
+        metavar='INT',
+        dest='accepted_queue_size',
+        type=int,
+        help='Maximum number of active requests in queue',
+    ),
+    '--accepted-queue-timeout': dict(
+        metavar='INT',
+        dest='accepted_queue_timeout',
+        type=int,
+        help='Timeout in seconds for putting requests into queue',
+    ),
+}
+
+
 def main():
     """Create a new Cheroot instance with arguments from the command line."""
     parser = argparse.ArgumentParser(
         description='Start an instance of the Cheroot WSGI/HTTP server.')
-    parser.add_argument(
-        '_wsgi_app', metavar='APP_MODULE',
-        type=str,
-        help='WSGI application callable or cheroot.server.Gateway subclass '
-        'to use')
-    parser.add_argument(
-        '--bind', metavar='ADDRESS',
-        dest='_bind_addr', type=str,
-        default='127.0.0.1:8000',
-        help='Network interface to listen on (default: 127.0.0.1:8000)')
-    parser.add_argument(
-        '--chdir', metavar='PATH',
-        type=os.chdir,
-        help='Set the working directory')
-    parser.add_argument(
-        '--server-name',
-        dest='server_name', type=str,
-        help='Web server name to be advertised via Server HTTP header')
-    parser.add_argument(
-        '--threads', metavar='INT',
-        dest='numthreads', type=int,
-        help='Minimum number of worker threads')
-    parser.add_argument(
-        '--max-threads', metavar='INT',
-        dest='max', type=int,
-        help='Maximum number of worker threads')
-    parser.add_argument(
-        '--timeout', metavar='INT',
-        dest='timeout', type=int,
-        help='Timeout in seconds for accepted connections')
-    parser.add_argument(
-        '--shutdown-timeout', metavar='INT',
-        dest='shutdown_timeout', type=int,
-        help='Time in seconds to wait for worker threads to cleanly exit')
-    parser.add_argument(
-        '--request-queue-size', metavar='INT',
-        dest='request_queue_size', type=int,
-        help='Maximum number of queued connections')
-    parser.add_argument(
-        '--accepted-queue-size', metavar='INT',
-        dest='accepted_queue_size', type=int,
-        help='Maximum number of active requests in queue')
-    parser.add_argument(
-        '--accepted-queue-timeout', metavar='INT',
-        dest='accepted_queue_timeout', type=int,
-        help='Timeout in seconds for putting requests into queue')
+    for arg, spec in _arg_spec.items():
+        parser.add_argument(arg, **spec)
     raw_args = parser.parse_args()
 
     # ensure cwd in sys.path
