d5cfcd0a4899cc64b7455b719075c4118f55d6fb
Jeff Van Voorst <jeff.vanvoorst@gmail.com>
2018-09-13 00:14:47 -0500

    update fixture -- try to get something that seems reasonable and is somewhat explicit

--- cheroot/test/conftest.py
+++ cheroot/test/conftest.py
@@ -35,14 +35,54 @@ def native_server_client(native_server):
 
 @pytest.fixture
 def wsgi_adapter(wsgi_server):
-    app = TestApp("http://localhost:{}".format(wsgi_server.bind_addr[1]))
+    # instruct TestApp to configure the WSGIProxy to use requests instead of httplib
+    app = TestApp("http://localhost:{}#requests".format(wsgi_server.bind_addr[1]))
     return pyriform.WSGIAdapter(app)
 
 @pytest.fixture
-def pyriform_session(wsgi_adapter):
+def wsgi_adapter_session(wsgi_adapter):
     session = requests.Session()
     session.mount('http://', adapter)
     return session
+
+
+@pytest.fixture
+def make_session_for_cheroot(wsgi_server):
+    '''
+    Get a factory function to create a pyriform adapter that links a
+    requests session to a wsgi_server fixture.  This is done by
+    having a WebTest TestApp use WSGIProxy2 to use an http library
+    to proxy directly to the given URL (for the web server -- in this
+    case the wsgi_server is the web server).  The httplib client doesn't
+    seem to handle a wsgi_server with an ssl_adapter other than None or a
+    webserver that is accessible via https.
+
+    :param wsgi_server:
+    :return: yields a function to create a pyriform session that directly
+             is proxied to the wsgi_server.  Depending on the args to the
+             yielded function, https can be enabled for the wsgi_server,
+             the wsgi_server can be mounted at a location other than 'http://',
+             and the http client used by the proxy object can be changed.
+    '''
+
+    saved_ssl_adapter = wsgi_server.ssl_adapter
+
+    def _make_session(prefix="http://", ssl_adapter=None, test_app_client="requests"):
+        scheme = "http"
+        assert prefix.startswith("http")
+        if prefix.startswith("https"):
+            assert ssl_adapter is not None
+            scheme = "https"
+
+        wsgi_server.ssl_adapter = ssl_adapter
+
+        app = TestApp("{}://localhost:{}#{}".format(scheme, wsgi_server.bind_addr[1], test_app_client))
+        return pyriform.make_session(app, prefix)
+
+    yield _make_session
+
+    wsgi_server.ssl_adapter = saved_ssl_adapter
+
 
 @pytest.fixture
 def crazy_wsgi_session(wsgi_server):
@@ -68,8 +108,9 @@ def crazy_wsgi_https_session(wsgi_server):
     ssl_adapter.context.verify_mode = ssl.CERT_NONE
     wsgi_server.ssl_adapter = ssl_adapter
     # instruct TestApp to configure the WSGIProxy to use requests instead of httplib
-    my_test_app = TestApp("https://127.0.0.1:{}#requests".format(wsgi_server.bind_addr[1]))
+    my_test_app = TestApp("https://localhost:{}#requests".format(wsgi_server.bind_addr[1]))
     adapter = pyriform.WSGIAdapter(my_test_app)
+    print ("adapter:", dir(adapter))
 
     # helpful to debug .....
     my_test_app.get("https://127.0.0.1:{}/".format(wsgi_server.bind_addr[1]))
--- cheroot/test/test_more.py
+++ cheroot/test/test_more.py
@@ -1,5 +1,7 @@
 import pytest
 import requests
+import ssl
+from cheroot.ssl.builtin import BuiltinSSLAdapter
 
 class TestMore(object):
 
@@ -12,5 +14,19 @@ class TestMore(object):
     def test_more_wsgi_server_two(self, crazy_wsgi_https_session):
         resp = crazy_wsgi_https_session.get('https://crazy_app2/')
         print ("inside test_more_wsgi_server_two, response is {}".format(resp))
+        assert 1 == 1
+        assert 1 == 0
+
+    def test_more_wsgi_server_three(self, make_session_for_cheroot):
+        ssl_adapter = BuiltinSSLAdapter(
+            certificate='test/ssl/server.cert',
+            private_key='test/ssl/server.key',
+            certificate_chain='test/ssl/ca.cert'
+        )
+        ssl_adapter.context.verify_mode = ssl.CERT_NONE
+        my_session = make_session_for_cheroot(prefix="https://try_this", ssl_adapter=ssl_adapter)
+        resp = my_session.get("https://try_this")
+        print("inside test_more_wsgi_server_three, response code is {}".format(resp))
+        print("inside test_more_wsgi_server_three, response text is:{}".format(resp.text))
         assert 1 == 1
         assert 1 == 0
