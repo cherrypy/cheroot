6e52f92ae9b3168f43af401d694eb048623ad613
Jeff Van Voorst <jeff.vanvoorst@gmail.com>
2018-09-26 21:26:07 -0500

    try this

--- cheroot/test/test_https.py
+++ cheroot/test/test_https.py
@@ -13,14 +13,14 @@ from cheroot.ssl.builtin import BuiltinSSLAdapter
 @pytest.fixture(scope='module')
 def SSLAdapter(root_ca, tmpdir_factory):
     """Given a testme CA, create SSL files and return a BuiltinSSLAdapter."""
-    server_cert = root_ca.issue_server_cert(u"localhost", u"127.0.0.1", u"::1")
+    server_cert = root_ca.issue_server_cert(u'localhost', u'127.0.0.1', u'::1')
 
-    server_dir = tmpdir_factory.mktemp("server")
-    server_cert_file = server_dir.join("server.cert")
+    server_dir = tmpdir_factory.mktemp('server')
+    server_cert_file = server_dir.join('server.cert')
     server_cert_file.write(server_cert.cert_chain_pems[0].bytes())
-    server_pkey_file = server_dir.join("server_pkey")
+    server_pkey_file = server_dir.join('server_pkey')
     server_pkey_file.write(server_cert.private_key_pem.bytes())
-    cert_chain_file = server_dir.join("ca.cert")
+    cert_chain_file = server_dir.join('ca.cert')
     cert_chain_file.write(root_ca.cert_pem.bytes())
 
     ssl_adapter = BuiltinSSLAdapter(
@@ -41,26 +41,26 @@ def client_cert_files(request, root_ca, tmpdir):
 
     cert_type = request.param
     cert_type_to_args = {
-        "client": [u"localhost", u"127.0.0.1", u"::1"],
+        'client': [u'localhost', u'127.0.0.1', u'::1'],
         # as far as I know, testme doesn't respect only ips for local host
-        "client_ip": [u"127.0.0.1", u"::1"],
-        "client_wildcard": [u"*.localhost"],
-        "client_wrong_host": [u"cherrypy.org", u"github.com"],
+        'client_ip': [u'127.0.0.1', u'::1'],
+        'client_wildcard': [u'*.localhost'],
+        'client_wrong_host': [u'cherrypy.org', u'github.com'],
     }
 
     client_cert = None
-    if cert_type == "client_wrong_ca":
+    if cert_type == 'client_wrong_ca':
         another_CA = trustme.CA()
         client_cert = \
-            another_CA.issue_server_cert(*cert_type_to_args["client"])
+            another_CA.issue_server_cert(*cert_type_to_args['client'])
     elif cert_type in cert_type_to_args:
         client_cert = root_ca.issue_server_cert(*cert_type_to_args[cert_type])
 
-    client_dir = tmpdir.mkdir("client")
+    client_dir = tmpdir.mkdir('client')
 
-    client_cert_file = client_dir.join("client.cert")
+    client_cert_file = client_dir.join('client.cert')
     client_cert_file.write(client_cert.cert_chain_pems[0].bytes())
-    client_pkey_file = client_dir.join("server_pkey")
+    client_pkey_file = client_dir.join('server_pkey')
     client_pkey_file.write(client_cert.private_key_pem.bytes())
     return CertKeyPair(str(client_cert_file), str(client_pkey_file), cert_type)
 
@@ -79,8 +79,8 @@ class _TestClientCertValidation:
             self, SSLAdapter, client_cert_files, make_session_for_cheroot):
         """Create a pyriform session to make an https request."""
         my_session = make_session_for_cheroot(
-            "localhost", prefix="https://", ssl_adapter=SSLAdapter,
-            test_app_client="requests", client_cert=client_cert_files.cert,
+            'localhost', prefix='https://', ssl_adapter=SSLAdapter,
+            test_app_client='requests', client_cert=client_cert_files.cert,
             client_key=client_cert_files.pkey)
 
         # The pyriform requests adaptor eats the request verify and cert args
