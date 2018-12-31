539523995e23ee7de9e1fc2056ce1168fc3cd3b2
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-31 21:47:39 +0100

    Patch pyopenssl+pypy3+macos expectation

--- cheroot/test/test_ssl.py
+++ cheroot/test/test_ssl.py
@@ -266,6 +266,8 @@ def test_tls_client_auth(
             'sslv3 alert bad certificate' if IS_LIBRESSL_BACKEND
             else 'tlsv1 alert unknown ca'
         )
+        if IS_PYPY and six.PY3 and IS_MACOS and adapter_type == 'pyopenssl':
+            expected_substring = 'tlsv1 alert unknown ca'
         assert expected_substring in err_text
 
 
