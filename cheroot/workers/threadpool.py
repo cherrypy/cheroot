b125a8e346f06cfce48733acbbb2e250b826fe5d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-05-24 22:42:11 +0200

    Use is_alive on threads instead of deprecated one
    Ref:
    https://travis-ci.org/cherrypy/cheroot/jobs/536951698#L440

--- cheroot/workers/threadpool.py
+++ cheroot/workers/threadpool.py
@@ -209,7 +209,7 @@ class ThreadPool:
         # Grow/shrink the pool if necessary.
         # Remove any dead threads from our list
         for t in self._threads:
-            if not t.isAlive():
+            if not t.is_alive():
                 self._threads.remove(t)
                 amount -= 1
 
@@ -242,7 +242,7 @@ class ThreadPool:
             endtime = time.time() + timeout
         while self._threads:
             worker = self._threads.pop()
-            if worker is not current and worker.isAlive():
+            if worker is not current and worker.is_alive():
                 try:
                     if timeout is None or timeout < 0:
                         worker.join()
@@ -250,7 +250,7 @@ class ThreadPool:
                         remaining_time = endtime - time.time()
                         if remaining_time > 0:
                             worker.join(remaining_time)
-                        if worker.isAlive():
+                        if worker.is_alive():
                             # We exhausted the timeout.
                             # Forcibly shut down the socket.
                             c = worker.conn
