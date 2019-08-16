d79366d097fa09b7041370bd9e0353666b95a611
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-08-17 01:33:13 +0200

    Expect ConnectionError in test_ssl under Windows

--- cheroot/test/test_ssl.py
+++ cheroot/test/test_ssl.py
@@ -319,6 +319,8 @@ def test_tls_client_auth(
         ) if PY34 else (
             requests.exceptions.SSLError,
         )
+        if six.PY3 and IS_WINDOWS:
+            expected_ssl_errors += requests.exceptions.ConnectionError
         with pytest.raises(expected_ssl_errors) as ssl_err:
             make_https_request()
 
