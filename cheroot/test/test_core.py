99314c6a1cd2afc9247fd748dec153e15e813792
Caleb Robinson <calebrob6@gmail.com>
2019-11-08 14:00:30 -0800

    Resetting changes from earlier commit 051669890b59a7a8f63c3d931009342bd25f06b5

--- cheroot/test/test_core.py
+++ cheroot/test/test_core.py
@@ -82,6 +82,7 @@ def testing_server(wsgi_server_client):
     """Attach a WSGI app to the given server and preconfigure it."""
     wsgi_server = wsgi_server_client.server_instance
     wsgi_server.wsgi_app = HelloController()
+    wsgi_server.max_request_body_size = 30000000
     wsgi_server.server_client = wsgi_server_client
     return wsgi_server
 
@@ -422,6 +423,7 @@ def testing_server_close(wsgi_server_client):
     """Attach a WSGI app to the given server and preconfigure it."""
     wsgi_server = wsgi_server_client.server_instance
     wsgi_server.wsgi_app = CloseController()
+    wsgi_server.max_request_body_size = 30000000
     wsgi_server.server_client = wsgi_server_client
     return wsgi_server
 
