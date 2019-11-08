051669890b59a7a8f63c3d931009342bd25f06b5
Caleb Robinson <calebrob6@gmail.com>
2019-11-08 12:59:36 -0800

    Removed max_request_body_size in test servers to reflect the conditions of a default server instance

--- cheroot/test/test_core.py
+++ cheroot/test/test_core.py
@@ -81,7 +81,6 @@ def testing_server(wsgi_server_client):
     """Attach a WSGI app to the given server and preconfigure it."""
     wsgi_server = wsgi_server_client.server_instance
     wsgi_server.wsgi_app = HelloController()
-    wsgi_server.max_request_body_size = 30000000
     wsgi_server.server_client = wsgi_server_client
     return wsgi_server
 
@@ -404,7 +403,6 @@ def testing_server_close(wsgi_server_client):
     """Attach a WSGI app to the given server and preconfigure it."""
     wsgi_server = wsgi_server_client.server_instance
     wsgi_server.wsgi_app = CloseController()
-    wsgi_server.max_request_body_size = 30000000
     wsgi_server.server_client = wsgi_server_client
     return wsgi_server
 
