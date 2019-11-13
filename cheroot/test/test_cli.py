a3eb3518e84b2c07c2321e9aa9b4d0c8be9b8a70
Joel Rivera <rivera@joel.mx>
2019-11-13 16:48:52 -0600

    Fix flake8 complains about spacing of test_cli.py

--- cheroot/test/test_cli.py
+++ cheroot/test/test_cli.py
@@ -3,18 +3,23 @@ from cheroot.cli import (
     parse_wsgi_bind_addr
 )
 
+
 def test_parse_wsgi_bind_location_for_tcpip():
     assert parse_wsgi_bind_addr('192.168.1.1:80') == ('192.168.1.1', 80)
     assert parse_wsgi_bind_addr('[::1]:8000') == ('::1', 8000)
 
+
 def test_parse_wsgi_bind_location_for_unix_socket():
     assert parse_wsgi_bind_addr('/tmp/cheroot.sock') == '/tmp/cheroot.sock'
+
 
 def test_parse_wsgi_bind_addr_for_abstract_unix_socket():
     assert parse_wsgi_bind_addr('@cheroot') == '\0cheroot'
 
+
 def test_Aplication_resolve():
     import sys
+
     class WSGIAppMock:
         def application(self):
             pass
