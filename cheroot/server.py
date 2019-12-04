4785d390305b81f8ba9b35e3ad863636d34540f5
RJ Seibert <alephnot0@gmail.com>
2019-12-04 00:24:15 -0500

    flake8 fixing over indentation

--- cheroot/server.py
+++ cheroot/server.py
@@ -824,8 +824,8 @@ class HTTPRequest:
             # `urlsplit()` above parses "example.com:3128" as path part of URI.
             # this is a workaround, which makes it detect netloc correctly
             uri_split = urllib.parse.urlsplit(
-                        '//{}'.format(uri)
-                        .encode('ascii'))
+                '//{}'.format(uri)
+                .encode('ascii'))
             _scheme, _authority, _path, _qs, _fragment = uri_split
             _port = EMPTY
             try:
