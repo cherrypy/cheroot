30e6b613b37976e42921f8ee1b78a47ddd4edb6a
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-05-06 14:04:48 +0100

    Avoid race condition on persistent HTTP connections
    Add a HTTP "Keep-Alive" header with "timeout" on the HTTP response
    to avoid a race condition on persistent HTTP connections when the
    HTTP client reuses a connection after the "socket.timeout" exception
    triggered on the HTTPServer but before the FIN packet is produced.
    
    When this happens, the client gets a "connection reset by peer" after
    writting the request.
    
    This commit makes a HTTP client to know about this "Keep-Alive" idle
    timeout by exposing it on the HTTP "Keep-Alive" response header, so
    the connection won't be reused if it was "idle" for that "timeout"
    after the last request response.

--- cheroot/server.py
+++ cheroot/server.py
@@ -1175,6 +1175,11 @@ class HTTPRequest:
                 if not self.close_connection:
                     self.outheaders.append((b'Connection', b'Keep-Alive'))
 
+        self.outheaders.append((
+            b'Keep-Alive',
+            "timeout={}".format(self.server.timeout).encode('ISO-8859-1'),
+        ))
+
         if (not self.close_connection) and (not self.chunked_read):
             # Read any remaining request body data on the socket.
             # "If an origin server receives a request that does not include an
