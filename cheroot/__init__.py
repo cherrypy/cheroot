11d383dd787387cc2f9d8e1c3e2b3daf93d6e5af
Jason R. Coombs <jaraco@jaraco.com>
2020-11-24 11:53:45 -0500

    Remove compatibility code from cheroot

--- cheroot/__init__.py
+++ cheroot/__init__.py
@@ -1,8 +1,5 @@
 """High-performance, pure-Python HTTP server used by CherryPy."""
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
 try:
     import pkg_resources
 except ImportError:
