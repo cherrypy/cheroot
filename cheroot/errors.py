99ed1d2decbc9ed6180a11e5ab8cdd8f1da5d8f7
Jason R. Coombs <jaraco@jaraco.com>
2020-11-24 12:03:24 -0500

    Remove compatibility code from errors

--- cheroot/errors.py
+++ cheroot/errors.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """Collection of exceptions raised and/or processed by Cheroot."""
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
 import errno
 import sys
 
