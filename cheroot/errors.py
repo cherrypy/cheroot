01e8e38c0ecf6a5b28941fcdf15c533383950888
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-11-30 18:51:58 +0100

    Clarify `ECONNRESET` explanation in cheroot.errors
    Ref #341
    Ref #344

--- cheroot/errors.py
+++ cheroot/errors.py
@@ -69,7 +69,7 @@ acceptable_sock_shutdown_error_codes = {
 * ENOTCONN — client is no longer connected
 * EPIPE — write on a pipe while the other end has been closed
 * ESHUTDOWN — write on a socket which has been shutdown for writing
-* ECONNRESET — connection is reset by the peer
+* ECONNRESET — connection is reset by the peer, we received a TCP RST packet
 
 Ref: https://github.com/cherrypy/cheroot/issues/341#issuecomment-735884889
 """
