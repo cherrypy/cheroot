cc394f33a8d0c9d2d8f82c0c8ea01ff91a6f4e2b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-12-06 01:47:39 +0100

    Fix "conns" spelling in cheroot.connections

--- cheroot/connections.py
+++ cheroot/connections.py
@@ -351,7 +351,7 @@ class ConnectionManager:
     def _num_connections(self):
         """Return the current number of connections.
 
-        Includes all conns registered with the selector,
+        Includes all connections registered with the selector,
         minus one for the server socket, which is always registered
         with the selector.
         """
