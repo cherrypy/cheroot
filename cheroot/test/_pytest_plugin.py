3eebb80bd7bd8a5860b5161571e817349c278345
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-01-05 03:17:37 +0100

    Dynamically add filtered warnings in pytest
    Ref #355

--- cheroot/test/_pytest_plugin.py
+++ cheroot/test/_pytest_plugin.py
@@ -11,19 +11,28 @@ from pytest import __version__
 
 
 pytest_version = tuple(map(int, __version__.split('.')))
-unraisable_exceptions = {
-    ('pytest', 'PytestUnhandledThreadExceptionWarning'),
-    ('pytest', 'PytestUnraisableExceptionWarning'),
-}
 
 
 def pytest_load_initial_conftests(early_config, parser, args):
     """Drop unfilterable warning ignores."""
-    if pytest_version >= (6, 2, 0):
+    if pytest_version < (6, 2, 0):
         return
 
-    early_config._inicache['filterwarnings'] = [
-        fw for fw in early_config.getini('filterwarnings')
-        if fw.count(':') < 2 or
-        tuple(fw.split(':')[2].split('.')[:2]) not in unraisable_exceptions
-    ]
+    # pytest>=6.2.0 under Python 3.8:
+    # Refs:
+    # * https://docs.pytest.org/en/stable/usage.html#unraisable
+    # * https://github.com/pytest-dev/pytest/issues/5299
+    early_config._inicache['filterwarnings'].extend((
+        'ignore:Exception in thread CP Server Thread-:'
+        'pytest.PytestUnhandledThreadExceptionWarning:_pytest.threadexception',
+        'ignore:Exception in thread Thread-:'
+        'pytest.PytestUnhandledThreadExceptionWarning:_pytest.threadexception',
+        'ignore:Exception ignored in. '
+        '<socket.socket fd=-1, family=AddressFamily.AF_INET, '
+        'type=SocketKind.SOCK_STREAM, proto=6>:'
+        'pytest.PytestUnraisableExceptionWarning:_pytest.unraisableexception',
+        'ignore:Exception ignored in. '
+        '<socket.socket fd=-1, family=AddressFamily.AF_INET6, '
+        'type=SocketKind.SOCK_STREAM, proto=6>:'
+        'pytest.PytestUnraisableExceptionWarning:_pytest.unraisableexception',
+    ))
