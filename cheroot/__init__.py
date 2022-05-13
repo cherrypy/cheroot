c5aeb841ef4e25b48aa7d0f0bce5876d7778964f
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-05-13 11:08:31 +0200

    __init__: generate correct version number
    Closes #506
    
    The distribution package number is reused as Python package version.
    
    This leads to errors in distutils.version.StrictVersion like
    
        ValueError: invalid version number '8.6.0+ds1'
    
    Use a regular expression to extract a valid version number.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- cheroot/__init__.py
+++ cheroot/__init__.py
@@ -1,6 +1,7 @@
 """High-performance, pure-Python HTTP server used by CherryPy."""
 
 from __future__ import absolute_import, division, print_function
+import re
 __metaclass__ = type
 
 try:
@@ -10,6 +11,9 @@ except ImportError:
 
 
 try:
-    __version__ = pkg_resources.get_distribution('cheroot').version
+    __version__ = re.search(
+        r'(?x)^(\d+)\.(\d+)(\.(\d+))?([ab](\d+))?',
+        pkg_resources.get_distribution('cheroot').version,
+    ).expand(r'\1.\2\3\5')
 except Exception:
     __version__ = 'unknown'
