e9629ac7b37587035ff6c62404e865743a98f5dd
Carey Metcalfe <carey@cmetcalfe.ca>
2022-07-08 08:18:24 -0400

    Deduplicate ThreadPool worker thread creation
    This patch refactors the `ThreadPool.start()` function to use the
    existing `ThreadPool.grow()` method to create the initial workers,
    reducing code duplication.

--- cheroot/workers/threadpool.py
+++ cheroot/workers/threadpool.py
@@ -173,17 +173,7 @@ class ThreadPool:
 
     def start(self):
         """Start the pool of threads."""
-        for _ in range(self.min):
-            self._threads.append(WorkerThread(self.server))
-        for worker in self._threads:
-            worker.name = (
-                'CP Server {worker_name!s}'.
-                format(worker_name=worker.name)
-            )
-            worker.start()
-        for worker in self._threads:
-            while not worker.ready:
-                time.sleep(.1)
+        self.grow(self.min)
 
     @property
     def idle(self):  # noqa: D401; irrelevant for properties
