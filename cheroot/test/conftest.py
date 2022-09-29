b910219d219c298539946ee963194c9b90a7e3d5
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-09-29 18:57:20 +0200

    Increase HTTP timeout in tests on slow CI VMs

--- cheroot/test/conftest.py
+++ cheroot/test/conftest.py
@@ -12,6 +12,7 @@ import time
 
 import pytest
 
+from .._compat import IS_MACOS, IS_WINDOWS
 from ..server import Gateway, HTTPServer
 from ..testing import (  # noqa: F401  # pylint: disable=unused-import
     native_server, wsgi_server,
@@ -22,7 +23,15 @@ from ..testing import get_server_client
 @pytest.fixture
 def http_request_timeout():
     """Return a common HTTP request timeout for tests with queries."""
-    return 0.1
+    computed_timeout = 0.1
+
+    if IS_MACOS: 
+        computed_timeout *= 2
+
+    if IS_WINDOWS:
+        computed_timeout *= 10
+
+    return computed_timeout
 
 
 @pytest.fixture
